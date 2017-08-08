1552b1f4a34d71151ed139ec3f0b33313eb38249
Ayush <ayush.aggarwal@knoldus.in>
2017-08-08 18:15:31 +0530

    Update File

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,46 +8,71 @@ import play.api.http.HttpEntity
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController extends Controller{
+class HomeController extends Controller {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("okkkkkkkk")
-   }
-
-  def defaultAction()= Action { implicit request: Request[AnyContent] =>
-    Result(
-      header = ResponseHeader(200)/*, Map(CONTENT_TYPE -> "text/plain")*/,
-      body = HttpEntity.Strict(ByteString("Hi play"), Some("text/plain"))
-    )
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index() = Action { implicit request =>
+    Ok(views.html.index())
   }
 
-  def plainTextAction()= Action { implicit request: Request[AnyContent] =>
-    Result(
-      header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
-      body = HttpEntity.Strict(ByteString("Hi this is plain text"), Some("text/plain"))
-    )
+
+  def receiveData() = Action { implicit request =>
+    val id = request.session.get("id")
+    val name = request.session.get("name")
+
+    val (keyOfMessage , message) = {
+      if (id.contains("hello") && name.contains("ayush"))
+        ("success", "Hi Ayush")
+      else
+        ("error", "ERROR")
+    }
+    Redirect(routes.HomeController.display()).flashing(keyOfMessage -> message)
   }
 
-  def htmlTextAction()= Action { implicit request: Request[AnyContent] =>
-    Result(
-      header = ResponseHeader(200,Map(CONTENT_TYPE->"text /html")),
-      body=HttpEntity.Strict(ByteString("This is html page"),Some("text /html"))
-    )
+
+  def sendInfo(id: String, name: String) = Action { implicit request =>
+
+
+    Redirect(routes.HomeController.receiveData()).withSession("id" -> s"$id", "name" -> s"$name")
+
   }
 
-  def redirectAction()= Action { implicit request: Request[AnyContent] =>
-    Redirect(routes.HomeController.plainTextAction()).flashing("message" -> "Success")
+  def display() = Action {
+    implicit request =>
+
+      Ok(views.html.message())
   }
+
+    def defaultAction()= Action { implicit request: Request[AnyContent] =>
+      Result(
+        header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+        body = HttpEntity.Strict(ByteString("Hi play"), Some("text/plain"))
+      )
+    }
+
+    def plainTextAction()= Action { implicit request: Request[AnyContent] =>
+      Result(
+        header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+        body = HttpEntity.Strict(ByteString("Hi this is plain text"), Some("text/plain"))
+      )
+    }
+
+    def htmlTextAction()= Action { implicit request: Request[AnyContent] =>
+      Result(
+        header = ResponseHeader(200,Map(CONTENT_TYPE->"text /html")),
+        body=HttpEntity.Strict(ByteString("This is html page"),Some("text /html"))
+      )
+    }
+
 
 }
